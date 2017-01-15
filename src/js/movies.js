a8542018f16422a4c85433af358f48e01cbbb271
Adam Bodie <aduo3000@yahoo.com>
2017-01-15 11:46:50 -0800

    Cleaned up Javascript, SASS

--- src/js/movies.js
+++ src/js/movies.js
@@ -3,7 +3,7 @@ let arr = [{
   "image": "blood-simple",
 	"year": 1984,
 	"starring": "John Getz, Frances McDormand, Dan Hedaya",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 3.5
 
 }, {
@@ -11,7 +11,7 @@ let arr = [{
   "image": "raising-arizona",
 	"year": 1987,
 	"starring": "Nicolas Cage, Holly Hunter",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.0
 
 }, {
@@ -19,21 +19,21 @@ let arr = [{
   "image": "millers-crossing",
 	"year": 1990,
 	"starring": "Gabriel Byrne, Marcia Gay Harden, John Turturro",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.0
 }, {
 	"title": "Barton Fink",
   "image": "barton-fink",
 	"year": 1991,
 	"starring": "John Turturro, John Goodman",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.0
 }, {
 	"title": "Fargo",
   "image": "fargo",
 	"year": 1996,
-	"starring": "Frances McDormand, William H.Macy, Steve Buscemi, Peter Stormare",
-	"review": "hipster ipsum",
+	"starring": "Frances McDormand, William H. Macy, Steve Buscemi, Peter Stormare",
+	"review": "Set in the Coen Brothers home state of Minnesota, Fargo tells the story of a Jerry Lundegaard (William H. Macy), who organizes a kidnap for ransom of his wife to extort money from his wealthy father-in-law, and the ensuing events that occur, which leads to police officer Marge Gunderson (Frances McDormand) of Brainerd to investigate.  Arguably the best film the Coen Brothers directed, as well as the film that brought them to mainstream success, Fargo is both dark and charming at the same time.  With the Minnesota winter backdrop and Minnesota Nice providing a perfect recipe of chaos, Fargo succeeds on all fronts, with Frances McDormand's performance as Marge Gunderson providing a human touch to an otherwise dark movie.  McDormand received the Academy Award for Best Actress for this film, and Fargo won the Academy Award for Best Original Screenplay, both of which were well deserved.  Even better was the stroke of genius decision by the Coen Brothers to introduce this film as 'This is a True Story,' which made the movie all the more riveting and controversial.  Fargo contains all the classic elements to the Coen Brothers filmography, from the eerie backdrop to the fascinating characters, making this film an easy classic.",
 	"stars": 5.0
 
 }, {
@@ -41,7 +41,7 @@ let arr = [{
   "image": "big-lebowski",
 	"year": 1998,
 	"starring": "Jeff Bridges, John Goodman, Julianne Moore, Steve Buscemi",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 5.0
 
 }, {
@@ -49,7 +49,7 @@ let arr = [{
   "image": "o-brother-where-art-thou",
 	"year": 2000,
 	"starring": "George Clooney, John Turturro, Tim Blake Nelson",
-	"review": "hipster ipsum",
+	"review": "The Coen Brothers released O Brother, Where Art Thou? in 2000.  Set in 1937 in Mississippi during the Great Depression, O Brother, Where Art Thou? is based on the classic Greek poem The Odyssey, written by Homer.  The story is of three convicts who escape from prison, and their quest for treasure.  One particularly great aspect of this film is the overall feel for the era, from the dull sepia-tinted background to the extensive use of folk music.  The soundtrack, which won the Grammy for Album of the Year in 2002, plays a key role in the film, in particular the hit song 'I Am A Man of Constant Sorrow.'  The movie's plot provides a modern take of The Odyssey, which is by no means revolutionary, but nonetheless, fitting for the film's perceived simpler times.  The acting of the film isn't mind-blowing, but at the end of the day, the three convicts, played by Clooney, Turturro and Nelson, are likeable enough, while there are interesting minor roles, played by the likes of Coen Brothers favorite John Goodman and Stephen Root.  In all, O Brother, Where Art Thou? is a charming and fun ride for those willing to take it.",
 	"stars": 4.5
 
 }, {
@@ -57,7 +57,7 @@ let arr = [{
   "image": "intolerable_cruelty",
 	"year": 2003,
 	"starring": "George Clooney, Catherine Zeta-Jones",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 2.5
 
 }, {
@@ -65,7 +65,7 @@ let arr = [{
   "image": "ladykillers",
 	"year": 2004,
 	"starring": "Tom Hanks, Irma Hall, Marlon Wayans",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 3.0
 
 }, {
@@ -73,7 +73,7 @@ let arr = [{
   "image": "no-country-for-old-men",
 	"year": 2007,
 	"starring": "Josh Brolin, Javier Bardem, Tommy Lee Jones",
-	"review": "hipster ipsum",
+	"review": "Set in West Texas in the 1980's, No Country For Old Men, based on a novel of the same name, tells the story of hunter Llewelyn Moss (Josh Brolin) who finds drug money, who's chased by sociopathic hitman Anton Chigurh (Javier Bardem), while Sheriff Ed Tom Bell (Tommy Lee Jones) investigates the events.  Dark and thrilling, No Country For Old Men is the Coen Brother's first (and only film to date) to win the Academy Award for Best Picture.  Javier Bardem's performance of Anton Chigurh is quite stunning and chilling, making Anton Chigurh the most shocking and fascinating character to be featured in any Coen Brothers film, as he easily won the Academy Award for Best Supporting Actor.  The cat and mouse chase between Llewelyn Moss and Anton Chigurh provides most of the suspense, while Ed Tom Bell provides the compassion, giving No Country For Old Men its complexity, while the bare western backdrop makes the story eerily calm.  No Country For Old Men is another classic for the Coen Brothers, who perfectly capture the consequences of greed and violence.",
 	"stars": 5.0
 
 }, {
@@ -81,7 +81,7 @@ let arr = [{
   "image": "burn-after-reading",
 	"year": 2008,
 	"starring": "Frances McDormand, Brad Pitt, George Clooney, John Malkovich, Tilda Swanson",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.0
 
 }, {
@@ -89,7 +89,7 @@ let arr = [{
   "image": "a-serious-man",
 	"year": 2009,
 	"starring": "Michael Stuhlbarg, Richard Kind, Fred Melamed",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.0
 
 }, {
@@ -97,41 +97,42 @@ let arr = [{
   "image": "true-grit",
 	"year": 2010,
 	"starring": "Jeff Bridges, Josh Brolin, Matt Damon",
-	"review": "hipster ipsum",
+	"review": "Fixie sunt mlkshk, flexitarian labore farm-to-table nisi salvia chicharrones culpa. Truffaut chicharrones small batch, qui butcher 8-bit DIY chillwave viral scenester. Bitters elit labore, subway tile tempor semiotics art party. Kogi jean shorts twee 8-bit tempor plaid. Street art snackwave ugh XOXO edison bulb, consectetur everyday carry post-ironic plaid. Intelligentsia organic forage four dollar toast lomo irure, art party nesciunt proident jianbing ugh delectus. Succulents raclette non photo booth live-edge cred health goth, butcher synth ut authentic.",
 	"stars": 4.5
 
 }]
 
-function createClasses(className, number) {
-  return className + number;
+var createButton = (index) => {
+ return $(`.button${index + 1}`).click(function(){
+		 $("#description").replaceWith(`
+			 <div id="description">
+			 	<div id="review">
+			 		<h1>${arr[index].title}</h1>
+					<div id="info">
+			 		<h4>${arr[index].year}</h4>
+			 		<h4>Starring ${arr[index].starring}</h4>
+			 		<p>${arr[index].review}</p>
+					</div>
+				</div>
+			 </div>`);
+ });
 }
-function createButton (index) {
-  return $(createClasses(".button", index + 1)).click(function(){
-      $("#description").replaceWith(`
-        <div id="description">
-        <h1>${arr[index].title}</h1>
-        <h4>${arr[index].year}</h4>
-        <h4>Starring ${arr[index].starring}</h4>
-        <p>${arr[index].review}</p>
-        </div>`);
-  });
-}
+$(function () {
+		for (var j = 0; j < arr.length; j++) {
+				$(`.rateYo${j+1}`).rateYo({rating: arr[j].stars, spacing: "5px"});
+		}
+});
+
 var txt = "";
 for (var i = 0; i < arr.length; i++) {
-    $(function () {
-        for (var j = 0; j < arr.length; j++) {
-            $(createClasses(".rateYo", j+1)).rateYo({rating: arr[j].stars, spacing: "5px"});
-        }
-    });
   txt += `<div class='movie'>
-              <img src="https://s3-us-west-2.amazonaws.com/film.bodiewebdesign.com/img/${arr[i].image}.jpg">
+              <img src="https://s3-us-west-2.amazonaws.com/film.bodiewebdesign.com/img/${arr[i].image}.jpg" class="img-responsive">
                     <div class='rateYo${i+1}'></div>
                     <button type="button" class="btn btn-primary button${i+1}">View Details</button>
           </div>
                     `
 }
 document.getElementById("movies").innerHTML = txt;
-
 
 for (var i = 0; i < arr.length; i++) {
     createButton(i);
