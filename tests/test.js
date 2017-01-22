ae444ddda04ac69ba8873260d855f186320634ed
Adam Bodie <aduo3000@yahoo.com>
2017-01-22 10:55:09 -0800

    Updated test suite

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,6 @@
 var chai = require('chai');
 var assert = chai.assert;    // Using Assert style
 var expect = chai.expect;    // Using Expect style
-var jquery = require('jquery');
 var year = 2017;
 var date = new Date("January 1, 2017 11:13:00");
 
@@ -21,16 +20,22 @@ describe('Copyright', function() {
 
 describe('Description', function() {
   var replaceDescription = require('../src/js/description.js');
+  var expectedValue = `<div id="description">\n\t <div id="review">\n\t\t <h1>Test</h1>\n\t\t <div id="info">\n\t\t <h4>1999</h4>\n\t\t <h4>Starring Stars</h4>\n\t\t <p>Review</p>\n\t\t </div>\n\t </div>\n\t</div>`;
   var arr = [{
   	"title": "Test",
     "image": "test",
   	"year": 1999,
   	"starring": "Stars",
   	"review": "Review",
-
+  },{
+  	"title": "Test2",
+    "image": "test2",
+  	"year": 1998,
+  	"starring": "Stars 2",
+  	"review": "Review 2",
   }]
   it('returns correct index', function() {
-    expect(replaceDescription(arr[0].title, arr[0].year, arr[0].starring, arr[0].review)).to.equal(`<div id="description">\n\t <div id="review">\n\t\t <h1>Test</h1>\n\t\t <div id="info">\n\t\t <h4>1999</h4>\n\t\t <h4>Starring Stars</h4>\n\t\t <p>Review</p>\n\t\t </div>\n\t </div>\n\t</div>`);
+    expect(replaceDescription(arr[0].title, arr[0].year, arr[0].starring, arr[0].review)).to.equal(expectedValue);
   });
 
 });
